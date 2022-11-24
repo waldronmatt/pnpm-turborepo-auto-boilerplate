15bfedf814fc058720e20e80cf4d83a2eb62754c
waldronmatt <mhuntwaldron@gmail.com>
2022-11-24 00:24:35 -0500

    fix(index.ts): add jsdoc for addition func

--- apps/test-app-addition/src/index.ts
+++ apps/test-app-addition/src/index.ts
@@ -1,2 +1,7 @@
+/**
+ * Addition Function
+ * @param {array} numbers - an array of numbers.
+ * @returns {number} Sum of numbers
+ */
 export const addition = (...a: number[]) =>
   a.reduce((accumulator, value) => accumulator + value, 0);
