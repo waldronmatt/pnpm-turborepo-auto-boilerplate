de844f88e4af61350f390c20341830007afd0eb5
waldronmatt <mhuntwaldron@gmail.com>
2022-11-24 00:26:01 -0500

    fix(index.ts): add jsdoc for subtraction function

--- apps/test-app-subtraction/src/index.ts
+++ apps/test-app-subtraction/src/index.ts
@@ -1,2 +1,7 @@
+/**
+ * Subtraction Function
+ * @param {array} numbers - an array of numbers.
+ * @returns {number} Result of subtracted numbers
+ */
 export const subtraction = (...a: number[]) =>
   a.length === 0 ? 0 : a.reduce((accumulator, value) => accumulator - value);
